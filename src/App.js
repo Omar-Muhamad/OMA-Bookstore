6ca78a960c211e0022934ec5ed6091c837193acc
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-01-19 16:50:02 +0200

    Add project components & redux files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Catagories from "./components/Catagories";
+import Books from "./components/Books";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>OMA Bookstore</h1>
+      <Header />
+      <Routes>
+        <Route path="/Catagories" element={<Catagories />} />
+        <Route exact path="/" element={<Books />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link>
+        <h1>OMA Bookstore</h1>
+      </Link>
+      <nav className="navbar">
+        <ul className="navLinks">
+          <Link to="/">
+            <li className="navLink">Books</li>
+          </Link>
+          <Link to="/Catagories">
+            <li className="navLink">Catagories</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

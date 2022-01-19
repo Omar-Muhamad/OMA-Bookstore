9c43446d53aaf3291d3762f885ac5c90b7df565f
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-01-19 19:20:36 +0200

    Add book list

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,13 @@
 const Books = () => (
   <>
-    <div>No Books</div>
+    <div>
+      <ul className="bookList">
+        <li>
+          <h3>Book Title</h3>
+          <button type="button" className="removeBtn">Remove</button>
+        </li>
+      </ul>
+    </div>
     <form>
       <input type="text" placeholder="Add new book..." name="title" required />
       <button type="submit">Add book</button>
